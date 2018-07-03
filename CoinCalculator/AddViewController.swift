919538bfa7f4561805da46e5ab64c0a3d1b1c5a4
Jacob Pashman <jacobpashman@gmail.com>
2018-07-03 08:51:27 -0700

    changes

--- CoinCalculator/AddViewController.swift
+++ CoinCalculator/AddViewController.swift
@@ -14,6 +14,7 @@ class AddViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+//        hidesBottomBarWhenPushed
         
         
     }
